f37e0f7cc2dfb06f3d293ced7e4d8481c5561ce2
Robin Rowe <Robin.Rowe@cinepaint.org>
2016-01-18 10:52:46 -0800

    Make Buffer::size a pointer

--- portable/Buffer.h
+++ portable/Buffer.h
@@ -13,11 +13,12 @@ namespace portable
 template <unsigned bufsize>
 class Buffer
 {	char buffer[bufsize];
-	unsigned size;
+	unsigned* size;
 public:
 	Buffer()	
-	{	buffer[0]=0;
-		size=0;
+	{	size=(unsigned*) buffer;
+		*size=sizeof(*size);
+		buffer[size]=0;
 	}
 	char* get() 
 	{	return buffer;
@@ -25,26 +26,29 @@ public:
 	const char* get() const
 	{	return buffer;
 	}
+	const char* data() const
+	{	return buffer+sizeof(*size);
+	}
 	unsigned capacity() const
 	{	return bufsize;
 	}
 	unsigned length() const
-	{	return size;
+	{	return *size;
 	}
 	bool Append(const char* data,unsigned length)
-	{	if (size + length > bufsize)
+	{	if (*size + length > bufsize)
 		{	return false;
 		}
-		memcpy(buffer+size,data,length);
-		size+=length;
+		memcpy(buffer+*size,data,length);
+		*size+=length;
 		return true;
 	}
 	bool Append(unsigned data)
 	{	return buffer.Append((const char*) &data,sizeof(data));
 	}
 	bool ends()
-	{	if(size<bufsize)
-		{	buffer[size]=0;
+	{	if(*size<bufsize)
+		{	buffer[*size]=0;
 			return true;
 		}
 		return false;
