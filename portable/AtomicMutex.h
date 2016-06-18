dc2e7127c4caeb005a53929e660597cf75887de7
Robin Rowe <Rrowe@ad.moves.com>
2016-06-17 19:38:32 -0700

    Added Timecode.h

---
+++ portable/AtomicMutex.h
@@ -0,0 +1,40 @@
+// libportable/AtomicMutex.h
+// Robin.Rowe@cinepaint.org 2014/11/20
+
+#ifndef AtomicMutex_h
+#define AtomicMutex_h
+
+#include <atomic>
+
+namespace portable
+{
+
+class AtomicMutex
+{	friend class SoftLock;
+	std::atomic<int> lock;
+public:
+	AtomicMutex()
+	{	lock=0;
+	}
+	bool IsLocked() const
+	{	return 1==lock;
+	}
+	bool operator!() const
+	{	return !IsLocked();
+	}
+	bool Lock()
+	{	const int lockCount=lock.fetch_add(1,std::memory_order_relaxed)+1;
+		const bool isLocked = (1==lockCount);
+		if(!isLocked)
+		{	Unlock();
+		}
+		return isLocked;
+	}
+	int Unlock()
+	{	const int lockCount=lock.fetch_sub(1,std::memory_order_relaxed)-1;
+		return lockCount;
+	}
+};
+
+}
+#endif
