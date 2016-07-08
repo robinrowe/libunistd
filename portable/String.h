61b8962116396dc1031bb3be3ec7def1120c5b42
Robin Rowe <robin@cienpaint.org>
2016-07-08 05:08:33 -0700

    Add String.h

---
+++ portable/String.h
@@ -0,0 +1,13 @@
+// ostring.h
+// replaces std::string and JavaString with stringz
+
+#ifndef ostring_h
+#define ostring_h
+
+//#include <string>
+//#include "stringz.h"
+#include "JavaString.h"
+
+//typedef std::string String;
+
+#endif
