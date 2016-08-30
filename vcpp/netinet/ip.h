47263a2053d4b2a245e8a4bf5ed19437a8547f1e
Robin Rowe` <robin.rowe@cinepaint.org>
2016-08-30 12:11:25 -0700

    ip.h u_int8_t

--- vcpp/netinet/ip.h
+++ vcpp/netinet/ip.h
@@ -22,7 +22,7 @@ extern "C" {
 struct ip
 {	unsigned int ip_hl:4;		/* header length */
 	unsigned int ip_v:4;		/* version */
-	u_int8_t ip_tos;			/* type of service */
+	uint8_t ip_tos;			/* type of service */
 	u_short ip_len;			/* total length */
 	u_short ip_id;			/* identification */
 	u_short ip_off;			/* fragment offset field */
@@ -30,8 +30,8 @@ struct ip
 	#define	IP_DF 0x4000			/* dont fragment flag */
 	#define	IP_MF 0x2000			/* more fragments flag */
 	#define	IP_OFFMASK 0x1fff		/* mask for fragmenting bits */
-	u_int8_t ip_ttl;			/* time to live */
-	u_int8_t ip_p;			/* protocol */
+	uint8_t ip_ttl;			/* time to live */
+	uint8_t ip_p;			/* protocol */
 	u_short ip_sum;			/* checksum */
 	struct in_addr ip_src, ip_dst;	/* source and dest address */
 };
