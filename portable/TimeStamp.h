1380161cf73a7b62a6ebb414ef906a58dc90ec41
Robin Rowe <robin@cinepaint.org>
2018-07-28 21:05:01 -0700

    Moved Now() to TimeStamp.h

--- portable/TimeStamp.h
+++ portable/TimeStamp.h
@@ -14,6 +14,7 @@
 #endif
 #include <time.h>
 #include <stdio.h>
+#include <string>
 
 namespace portable {
 
@@ -260,9 +261,32 @@ public:
 		microseconds = mktime(&t)*factor;//not timegm
 	}
 #endif
-
 };
 
+#ifdef _WIN32
+
+inline
+std::string Now()
+{	FILETIME ft;
+	GetSystemTimeAsFileTime(&ft);
+	SYSTEMTIME st;
+	FileTimeToSystemTime(&ft,&st);
+// 2014-11-22 12:45:34.001
+	const unsigned BUFSIZE=30;
+	char buffer[BUFSIZE];
+	sprintf_s(buffer,BUFSIZE,"%04d-%02d-%02d %02d:%02d:%02d.%03d",
+		st.wYear,
+		st.wMonth,
+//		st.wDayOfWeek,
+		st.wDay,
+		st.wHour,
+		st.wMinute,
+		st.wSecond,
+		st.wMilliseconds);
+	return buffer;
+}
+
+#endif
 }
 
 #endif
