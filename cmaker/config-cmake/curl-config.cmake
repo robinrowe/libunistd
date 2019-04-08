e348a5744c02a7e54b1cc59b768657758f14b39d
Robin Rowe <robin.rowe@cinepaint.org>
2019-04-08 19:45:46 -0400

    Updated curl-config.cmake with Windows SSL flag

--- cmaker/config-cmake/curl-config.cmake
+++ cmaker/config-cmake/curl-config.cmake
@@ -2,9 +2,15 @@
 # libssh2/cmake/curl-config.cmake
 # Edit curl/CMakeLists.txt:
 #   find_package(curl REQUIRED CONFIG)
-# cmake -DENABLE_curl_COMPRESSION=ON -Dcurl_DIR=../cmake .. -A x64
+# cmake -DENABLE_curl_COMPRESSION=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_USE_WINSSL=ON .. -A x64
 
 set(curl_INCLUDE_DIR /code/curl/include)
-set(curl_LIBRARY /code/curl/build/lib/Debug/libcurl-d_imp.lib)
+set(curl_LIBRARY /code/curl/build/lib/Debug/libcurl-d.lib)
 set(curl_INCLUDE_DIRS ${curl_INCLUDE_DIR})
 set(curl_LIBRARIES ${curl_LIBRARY})
+
+set(CURL_LIBS 
+	wldap32
+	crypt32.lib
+	Ws2_32.lib
+)
