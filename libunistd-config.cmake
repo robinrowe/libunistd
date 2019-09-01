c0706e60092d8f281df6c479007056ab82d80100
Robin Rowe <robin@cinepaint.org>
2019-08-31 18:22:21 -0700

    cmake config fixes

--- libunistd-config.cmake
+++ libunistd-config.cmake
@@ -11,26 +11,32 @@
 message("--- Found libunistd! ---")
 message("  CMAKE_CURRENT_LIST_DIR = ${CMAKE_CURRENT_LIST_DIR}")
 
-set(LIBUNISTD_INCLUDE_DIRS ${CMAKE_CURRENT_LIST_DIR}/unistd)
-set(LIBUNISTD_LIBRARIES libunistd.lib)
-set(LIBUNISTD_LIB_DIRS ${CMAKE_CURRENT_LIST_DIR})
-
-if (NOT DEFINED CMAKE_STATIC_LIBRARY_SUFFIX)
+#if (NOT DEFINED CMAKE_STATIC_LIBRARY_SUFFIX)
     if (WIN32)
+        set(CMAKE_STATIC_LIBRARY_PREFIX "")
         set(CMAKE_STATIC_LIBRARY_SUFFIX ".lib")
     else()
         set(CMAKE_STATIC_LIBRARY_PREFIX "lib")
         set(CMAKE_STATIC_LIBRARY_SUFFIX ".a")
     endif()
-endif()
+#endif()
+
+set(LIBUNISTD_LIBRARIES ${CMAKE_STATIC_LIBRARY_PREFIX}unistd${CMAKE_STATIC_LIBRARY_SUFFIX})
 
 if(${LIBUNISTD_DOWNLOAD})
+	set(LIBUNISTD_LIB_DIRS ${CMAKE_CURRENT_LIST_DIR})
+	set(LIBUNISTD_INCLUDE_DIRS ${CMAKE_CURRENT_LIST_DIR}/unistd)
+# Adjusting LIBUNISTD_DOWNLOAD path 
+#   C:/Code/gitlab/ccLater/build/win64/libunistd/libunistd-src...
+#   /c/code/gitlab/cclater/build/win64/build/win64/libunistd/libunistd-src/unistd/Debug/libunistd.lib
 	message("Adjusting LIBUNISTD_DOWNLOAD path ${LIBUNISTD_LIB_DIRS}...")
 	set(LIBUNISTD_INCLUDE_DIRS ${CMAKE_CURRENT_LIST_DIR})
 	set(LIBUNISTD_LIB_DIRS 
 		debug ${LIBUNISTD_LIB_DIRS}/build/win64/unistd/Debug
 		optimized ${LIBUNISTD_LIB_DIRS}/build/win64/unistd/Release ) 
 else(${LIBUNISTD_DOWNLOAD})
+	set(LIBUNISTD_LIB_DIRS ${CMAKE_CURRENT_LIST_DIR})
+	set(LIBUNISTD_INCLUDE_DIRS ${CMAKE_CURRENT_LIST_DIR})
 	set(LIBUNISTD_LIB_DIRS 
 		debug ${LIBUNISTD_LIB_DIRS}/unistd/Debug
 		optimized ${LIBUNISTD_LIB_DIRS}/unistd/Release ) 
