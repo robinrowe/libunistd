1971c083898ca24bedce54daf42ba8531cb974ba
Tim Wojtulewicz <tim@corelight.com>
2023-02-08 09:59:16 -0700

    Add net/ethertypes.h for a few ETHERTYPE_ definitions
    These definitions usually live in net/ethernet.h on GNU systems.

---
+++ unistd/net/ethertypes.h
@@ -0,0 +1,10 @@
+// net/ethertypes.h
+// Provided by brimdata on 2022/10/19
+// Copyright (c) 2019 Robin.Rowe@CinePaint.org
+// License open source MIT
+
+#pragma once
+
+#define ETHERTYPE_IP     0x0800
+#define ETHERTYPE_ARP    0x0806
+#define ETHERTYPE_REVARP 0x8035
