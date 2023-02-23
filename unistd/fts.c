7537cd9ef49883744149dfdae88cd26892de454a
Tim Wojtulewicz <timwoj@gmail.com>
2023-02-23 11:26:01 -0700

    Fix error in usage of stub methods

--- unistd/fts.c
+++ unistd/fts.c
@@ -5,14 +5,16 @@
 #include "fts.h"
 #include <stddef.h>
 
+#include "../portable/stub.h"
+
 FTS *fts_open(char * const *path_argv, int options, int (*compar)(const FTSENT **, const FTSENT **))
 {	STUB_0(fts_open);
 }
 
 FTSENT *fts_read(FTS *ftsp)
-{	return STUB_0(fts_read);
+{	STUB_0(fts_read);
 }
 
 int fts_close(FTS *ftsp)
-{	return STUB_NEG(fts_close);
+{	STUB_NEG(fts_close);
 }
