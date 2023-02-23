e7e523c9dd483f85373ba4caba72cd92a89ff6c6
Tim Wojtulewicz <timwoj@gmail.com>
2023-02-23 12:02:04 -0700

    Link to shlwapi from CMakeLists, not via pragma

--- unistd/fnmatch.cpp
+++ unistd/fnmatch.cpp
@@ -5,8 +5,6 @@
 #include <Shlwapi.h>
 #include "fnmatch.h"
 
-#pragma comment(lib, "shlwapi.lib")
-
 int fnmatch(const char *pattern, const char *string, int flags)
 {	BOOL match = PathMatchSpecA(string, pattern); // flags are not supported in Windows
 	return match ? 0 : FNM_NOMATCH;
